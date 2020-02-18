4c62ce266c2fc0820437bbb283767711cfe868f6
Vanessa McHale <vamchale@gmail.com>
2020-02-18 10:31:47 -0600

    maybe data

---
+++ dump-csv.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_digits
+
+from skimage import measure
+
+digits = load_digits()
+
+first = digits.images[0].reshape((8, 8))
+
+contours = measure.find_contours(first, 0.8)
