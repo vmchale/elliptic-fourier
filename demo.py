ef05586621f153a970824c0b75135a7e44be2d0e
Vanessa McHale <vamchale@gmail.com>
2020-04-05 10:54:52 -0500

    remove demo.py (works)

--- demo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pyefd
-
-triangle = np.array([[0,0],[4,0],[4,3],[0,0]])
-print(triangle.shape)
-offset = pyefd.calculate_dc_coefficients(triangle)
-print(offset)
-coeffs = pyefd.elliptic_fourier_descriptors(triangle, order=5)
-print(coeffs)
